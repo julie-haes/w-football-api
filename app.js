74660a245bf9bdcb123f3bcb7e879538a288b229
Julie Haes <juliehaesendonckx@hotmail.com>
2017-08-27 15:06:05 +0200

    add all routes to "app.js"

--- app.js
+++ app.js
@@ -13,15 +13,27 @@ app.use(bodyParser({ extended: true }));
 app.use(bodyParser.json());
 
 // Routes
+var games = require('./routes/games.js');
 var teams = require('./routes/teams.js');
-var countries = require('./routes/countries.js');
+var events = require('./routes/events.js');
 var players = require('./routes/players.js');
+var palmares = require('./routes/palmares.js');
+var countries = require('./routes/countries.js');
+var selections = require('./routes/selections.js');
+var tournaments = require('./routes/tournaments.js');
 var competitions = require('./routes/competitions.js');
+var tournamentGames = require('./routes/tournamentGames.js');
 
+app.use('/games', games);
 app.use('/teams', teams);
-app.use('/countries', countries);
+app.use('/events', events);
 app.use('/players', players);
+app.use('/palmares', palmares);
+app.use('/countries', countries);
+app.use('/selections', selections);
+app.use('/tournaments', tournaments);
 app.use('/competitions', competitions);
+app.use('/tournamentGames', tournamentGames);
 
 // Catch 404 Errors and forward them to error handler
 app.use((req, res, next) => {
